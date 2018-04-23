be75a25c753e301618a2670849eec2114154c083
JON <Jcribarro@gmail.com>
2018-04-23 03:03:40 -0400

    Took me about 20 minutes to figure out I needed a Box collider in addition to the rigidbody to actually get through a trigger check, but I saw somewhere in a Unity forum that I didn't need that. Real nice , real classy, making games is my apsssion

--- Assets/Scripts/game Scripts/changeReticleColor.cs
+++ Assets/Scripts/game Scripts/changeReticleColor.cs
@@ -5,14 +5,10 @@ using UnityEngine.UI;
 public class changeReticleColor : MonoBehaviour {
 
     [SerializeField] private SpriteRenderer spriteToColor;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+	 
 	// Update is called once per frame
 	void Update () {
-        this.gameObject.GetComponent<RawImage>().color = new Color(1.0f, 0.0f, 0.0f);
+        spriteToColor.GetComponent<SpriteRenderer>().color = new Color(1.0f, 0.0f, 0.0f);
 	}
     
 
@@ -21,9 +17,11 @@ public class changeReticleColor : MonoBehaviour {
         if(collision.tag == "meteor")
         {
             Debug.Log("Hit the meteor");
-            this.gameObject.GetComponent<RawImage>().color = new Color(0.0f, 1.0f, 0.0f);
+            spriteToColor.GetComponent<SpriteRenderer>().color = new Color(0.0f, 1.0f, 0.0f);
             
         }
     }
+    
+    //try an onTriggerStay, the enter doesnt seem to be working
 
 }
